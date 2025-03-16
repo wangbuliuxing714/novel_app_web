98edf79a3f125dfe9adc7c226ce3ae3e07b7f1eb
wangbuliuxing714 <wangbuliuxing154@gmail.com>
2025-03-16 18:10:09 +0800

    更新：修复上下文限制处理功能，优化双模型会话ID共享机制

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "c47233d7fb7a250d887c2e91ed3c9f8f
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "a423bea6cfabe12093405f99c62e7e2b",
+"flutter_bootstrap.js": "9bc39b854a45d0cfc27c22367ddbf150",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4ee13854f22586048db216372526b43c",
-"/": "4ee13854f22586048db216372526b43c",
-"main.dart.js": "4a8eb1468cab575d355a8ac08c822a77",
+"index.html": "07524f8db194dc7c5fcbc162074d5686",
+"/": "07524f8db194dc7c5fcbc162074d5686",
+"main.dart.js": "d1f374dcc5a616efffe646c1a570fe29",
 "manifest.json": "6483819f51c85c785bb74b3f64cf7ae1",
 "version.json": "c6bbde0251a74e15586702358bb44c0f"};
 // The application shell files that are downloaded before a service worker can
