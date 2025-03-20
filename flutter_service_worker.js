c0e88993724a2a8e853e377f1d451908bedbee3a
wangbuliuxing714 <wangbuliuxing154@gmail.com>
2025-03-20 19:30:05 +0800

    修复Flutter初始化脚本，解决Flutter未定义的问题

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,17 +27,16 @@ const RESOURCES = {"assets/AssetManifest.bin": "c47233d7fb7a250d887c2e91ed3c9f8f
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "4d1e6a60d47cded58f058f05a70a8b48",
+"flutter_bootstrap.js": "36c51b49d77f4e577ca36247eb6eddea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "188de4dedf5ac68808812f33426255d0",
-"/": "188de4dedf5ac68808812f33426255d0",
+"index.html": "4d9001b2cd1687cf1807e7480c7a712f",
+"/": "4d9001b2cd1687cf1807e7480c7a712f",
 "main.dart.js": "32336fb1c295ad0aac6a7c06f6ffb5a8",
 "manifest.json": "6483819f51c85c785bb74b3f64cf7ae1",
-"version.json": "c6bbde0251a74e15586702358bb44c0f",
-"_redirects": "24451388c2fc69419fac47ca08311257"};
+"version.json": "c6bbde0251a74e15586702358bb44c0f"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
